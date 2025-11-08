12519fc301ccf34e19751a08df3bae6f6cce0750
Andrew Zubkov <99863211+METALROK@users.noreply.github.com>
2025-11-08 20:50:59 +0300

    Add files via upload

---
+++ Trainings 1.0 - Classic ML/HomeWork 1 - kNN+Laplas/Task 2 - Laplas/distribution.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+class LaplaceDistribution:    
+    @staticmethod
+    def mean_abs_deviation_from_median(x: np.ndarray):
+        '''
+        Args:
+        - x: A numpy array of shape (n_objects, n_features) containing the data
+          consisting of num_train samples each of dimension D.
+        '''
+        ####
+        # Do not change the class outside of this block
+        # Your code here
+        ####
+        u = np.median(x, axis=0)
+        deviation = np.abs(x - u)
+        mad = np.mean(deviation, axis=0)
+        return mad
+
+    def __init__(self, features):
+        '''
+        Args:
+            feature: A numpy array of shape (n_objects, n_features). Every column represents all available values for the selected feature.
+        '''
+        ####
+        # Do not change the class outside of this block
+        self.loc = np.median(features, axis=0) # YOUR CODE HERE
+        self.scale = self.mean_abs_deviation_from_median(features) # YOUR CODE HERE
+        ####
+
+
+    def logpdf(self, values):
+        '''
+        Returns logarithm of probability density at every input value.
+        Args:
+            values: A numpy array of shape (n_objects, n_features). Every column represents all available values for the selected feature.
+        '''
+        ####
+        # Do not change the class outside of this block
+        abs_dev = np.abs(values - self.loc)
+        term_abs_dev_scaled = -abs_dev / self.scale
+        constant_term = -np.log(2) - np.log(self.scale)
+        log_density = constant_term + term_abs_dev_scaled
+        return log_density
+        ####
+        
+    
+    def pdf(self, values):
+        '''
+        Returns probability density at every input value.
+        Args:
+            values: A numpy array of shape (n_objects, n_features). Every column represents all available values for the selected feature.
+        '''
+        return np.exp(self.logpdf(values))
